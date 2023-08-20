d54686939e565e1c3848076f4ecf1c6afe1ee633
MichaelReyes1 <142757018+MichaelReyes1@users.noreply.github.com>
2023-08-20 16:48:20 -0700

    Add files via upload

---
+++ treasurehunt.js
@@ -0,0 +1,42 @@
+var randomTreasure = Math.floor(Math.random()*9)
+var randomBomb = Math.floor(Math.random()*9)
+if(randomTreasure === randomBomb){
+  randomBomb = Math.floor(Math.random()*9)
+}
+
+//define what you want your bomb and treasure to be then define what you want everything else to be
+// randomTreasure = "🌈"
+// randomBomb = "💣"
+// if not randomTreasure and randomBomb = "🌴"
+
+
+const changeToEmoji = (number) => {
+    console.log('treasure', randomTreasure)
+    console.log('bomb', randomBomb)
+    console.log(number)
+    if (randomTreasure === number){
+        document.getElementById(number).innerHTML = "🌈"
+        alert("You are the Winner")
+    } else if (randomBomb === number){
+        document.getElementById(number).innerHTML = "💣"
+        alert("You got the Bomb!")
+    } else if (randomTreasure !== number && randomBomb !== number){
+        document.getElementById(number).innerHTML = "🌴"
+    }
+}
+ //logical and --&&
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
